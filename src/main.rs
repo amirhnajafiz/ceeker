01f54d8145c09b5eb9003863a22ccf0b6bc00d6f
amirhnajafiz <najafizadeh21@gmail.com>
2025-06-01 13:28:10 -0400

    add: config loads from env

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use config::Config;
+use serde::Deserialize;
+use dotenv::dotenv;
+use std::env;
+
+#[derive(Debug, Deserialize)]
+struct Settings {
+    host: String,
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok(); // Loads from .env file into std::env
+
+    let settings = Config::builder()
+        .add_source(config::Environment::default()) // reads from env vars
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+
+    println!("Host: {}, Port: {}", settings.host, settings.port);
 }
 
